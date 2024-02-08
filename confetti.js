c73fba4f2d12c8cd52be79fbb9513145aaaea295
shraeya-iyer <126832460+shraeya-iyer@users.noreply.github.com>
2024-02-08 00:01:50 -0600

    Create confetti.js for valentines_day

---
+++ confetti.js
@@ -0,0 +1,26 @@
+function startConfetti() {
+    var end = Date.now() + (15 * 1000);
+
+    var colors = ['#bb0000', '#ffcccc'];
+
+    (function frame() {
+        confetti({
+            particleCount: 2,
+            angle: 60,
+            spread: 55,
+            origin: { x: 0 },
+            colors: colors
+        });
+        confetti({
+            particleCount: 2,
+            angle: 120,
+            spread: 55,
+            origin: { x: 1 },
+            colors: colors
+        });
+
+        if (Date.now() < end) {
+            requestAnimationFrame(frame);
+        }
+    }());
+}
